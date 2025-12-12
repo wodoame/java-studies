9f96dad32c0960c2c73f52192064311098240cf1
Wodoame <bmwodoame@gmail.com>
2025-12-12 14:30:50 +0000

    remove resources directory, add other changes

---
+++ DSA/src/Department.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Department {
+    public String name;
+    public List<Department> subDepartments;
+    public List<Integer> interns;
+    public List<Integer> managers;
+
+    public Department() {
+        this.subDepartments = new ArrayList<>();
+        this.interns = new ArrayList<>();
+        this.managers = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "name='" + name + '\'' +
+                ", subDepartments=" + subDepartments +
+                ", interns=" + interns +
+                ", managers=" + managers +
+                '}';
+    }
+}
---
+++ DSA/src/MegaCorp.java
@@ -0,0 +1,76 @@
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class MegaCorp {
+    public static void main(String[] args) {
+        Department root = loadData("src/company.json");
+        if (root != null) {
+//            System.out.println("Successfully loaded data for: " + root.name);
+
+            boolean found = checkForEmployee(root, 0); // Intern in Head Office & Mobile
+            if(!found){
+                System.out.println("Employee not found");
+            }
+        }
+    }
+
+    public static Department loadData(String filename) {
+        try (Reader reader = Files.newBufferedReader(Paths.get(filename))) {
+            Gson gson = new Gson();
+            return gson.fromJson(reader, Department.class);
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public static boolean checkForEmployee(Department dept, int id) {
+        // Check current department
+        if (searchInterns(dept.interns, id)) {
+            System.out.println("Found Intern " + id + " in " + dept.name);
+            return true;
+        }
+        if (searchManagers(dept.managers, id)) {
+            System.out.println("Found Manager " + id + " in " + dept.name);
+            return true;
+        }
+
+        // Recurse
+        for (Department sub : dept.subDepartments) {
+            if(checkForEmployee(sub, id)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean searchInterns(List<Integer> interns, int target) {
+        for (Integer intern : interns) {
+            if (intern == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean searchManagers(List<Integer> managers, int target) {
+        int left = 0;
+        int right = managers.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int val = managers.get(mid);
+            if (val == target) {
+                return true;
+            } else if (val < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
