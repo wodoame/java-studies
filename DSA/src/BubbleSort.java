2ac4525668fdba93ee48e24af69319f9a62fcc1b
Wodoame <bmwodoame@gmail.com>
2025-12-18 16:29:45 +0000

    changes

---
+++ DSA/src/BubbleSort.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+     int[] nums = {6, 1, 2, 3, 4, 5};
+     bubbleSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    static void bubbleSort(int[] nums){
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if(nums[j] > nums[j + 1]){
+                    swap(nums, j, j + 1);
+                }
+            }
+        }
+    }
+
+    static void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
---
+++ notes/commandprof/essential-written-communication.md
@@ -0,0 +1,10 @@
+Key things:
+1. Communication channels (email, reports, proposals, policies, etc)
+2. Clarity and conciseness
+3. Writing efficiency
+
+According to communication theorist `David Berlo` (1960, SMCR model), the `effectiveness` of any written message depends on four key factors:
+1. Source (the writer)
+2. Message
+3. Channel
+4. Receiver (the reader)
