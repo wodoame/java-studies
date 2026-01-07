bb9fd0176a0aa53cd5b6bec14c3a75aaa0f20052
wodoame <bmwodoame@gmail.com>
2026-01-07 22:33:21 +0000

    changes

---
+++ DSA/src/leetcode/arrays/ProductOfArrayExceptSelf.java
@@ -0,0 +1,12 @@
+package leetcode.arrays;
+
+import java.util.Arrays;
+
+public class ProductOfArrayExceptSelf {
+    static void main() {
+        System.out.println(Arrays.toString(productExceptSelf( new int[]{1,2,3,4})));
+    }
+    public static int[] productExceptSelf(int[] nums) {
+       return new int[]{};
+    }
+}
