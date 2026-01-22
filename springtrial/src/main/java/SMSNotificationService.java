8984be7290f92a9b1f51fdb30a5105b62f5a542a
Wodoame <bmwodoame@gmail.com>
2026-01-22 16:32:23 +0000

    delete springtrial project

--- springtrial/src/main/java/SMSNotificationService.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.stereotype.Service;
-
-@Service("sms")
-public class SMSNotificationService implements NotificationService{
-    @Override
-    public void send() {
-        System.out.println("Sent email using " + SMSNotificationService.class);
-    }
-}
