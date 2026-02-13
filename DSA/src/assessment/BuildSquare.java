8a50590a50b5e670f05bfb8602bb12c7fd2bbeb3
Wodoame <bmwodoame@gmail.com>
2026-02-13 16:25:04 +0000

    solve some DSA problems

--- DSA/src/assessment/BuildSquare.java
+++ DSA/src/assessment/BuildSquare.java
@@ -1,4 +1,20 @@
 package assessment;
 
+import java.util.Arrays;
+
 public class BuildSquare {
+    static void main() {
+        for(String row: buildSquare(8)){
+            System.out.println(row);
+        }
+    }
+    public static String[] buildSquare(int n){
+       String[] res = new String[n];
+       res[0] = "*".repeat(n);
+       res[n - 1] = "*".repeat(n);
+       for (int i = 1; i < n - 1; i++) {
+           res[i] = "*" + " ".repeat(n - 2) + "*";
+       }
+       return res;
+    }
 }
--- DSA/src/assessment/ScooterQuestion.java
+++ DSA/src/assessment/ScooterQuestion.java
@@ -1,4 +1,25 @@
 package assessment;
 
+import java.util.Arrays;
+
 public class ScooterQuestion {
+    static void main() {
+        System.out.println(solution(23, new int[]{10, 20}));
+    }
+
+    public static int solution(int finish, int[] scooters) {
+        Arrays.sort(scooters);
+        int totalDistance = 0;
+        int pos = 0;
+        int next = 0;
+        while(pos < finish){
+            while(next < scooters.length && scooters[next] < pos)next++;
+            if(next >= scooters.length)break;
+            pos = scooters[next];
+            int mov = Math.min(10, finish - pos);
+            pos+= mov;
+            totalDistance += mov;
+        }
+        return totalDistance;
+    }
 }
