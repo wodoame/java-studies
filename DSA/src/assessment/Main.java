ef24f4ffef40098d1b9d14129b6493208ae08072
Wodoame <bmwodoame@gmail.com>
2026-02-16 16:31:27 +0000

    changes

--- DSA/src/assessment/Main.java
+++ DSA/src/assessment/Main.java
@@ -1,11 +1,24 @@
 package assessment;
 
-import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
 
 public class Main {
      static void main(String[] args) {
-        int[] nums = {1, 3, 5, 2};
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-    }
+         // Supplier has the method get() that returns a value of type T
+         // Runnable has the method run() that does not return a value
+         // Consumer has the method accept(T t) that takes a value of type T and returns void
+         // Function has the method apply(T t) that takes a value of type T and returns a value of type R
+         // Predicate has the method test(T t) that takes a value of type T and returns a boolean
+
+         // thenRun, thenAccept, thenApply, thenCompose, thenCombine
+         var future = CompletableFuture.runAsync(()->{
+             try {
+                 Thread.sleep(1000);
+             } catch (InterruptedException e) {
+                 e.printStackTrace();
+             }
+         });
+         future.thenRun(()-> System.out.println("Task completed"));
+         CompletableFuture.supplyAsync(()-> 1).thenAccept((j)-> System.out.println("Hey"));
+ }
 }
--- fproj/src/demo/Main.java
+++ fproj/src/demo/Main.java
@@ -2,11 +2,6 @@ package demo;
 
 public class Main {
     public static void main(String[] args) throws IllegalAccessException {
-        UserProfile user1 = new UserProfile("JavaDev123"); // 10 chars - OK
-        UserProfile user2 = new UserProfile("Neovim_Enthusiast"); // 17 chars - FAIL
-        
-        Validator.validate(user1);
-        System.out.println("User 1 is valid!");
-        Validator.validate(user2); // This will throw the exception
+
     }
 }
