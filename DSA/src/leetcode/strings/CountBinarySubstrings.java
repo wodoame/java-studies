da580bac7138afd17bc476ae62cf68afc29ab657
Wodoame <bmwodoame@gmail.com>
2026-02-20 16:16:38 +0000

    WIP: solve leetcode 696

---
+++ DSA/src/leetcode/strings/CountBinarySubstrings.java
@@ -0,0 +1,12 @@
+package leetcode.strings;
+
+// leetcode 696
+public class CountBinarySubstrings {
+    static void main() {
+        System.out.println(countBinarySubstrings("00110011"));
+    }
+    public static int countBinarySubstrings(String s) {
+       // TODO: // Try to solve this
+       return 0;
+    }
+}
